7403c7f3554a6b6b8364f884b2e4966da73aedcf
Dmitry Sokolov <dmitry@scrofa-tridens.ru>
2017-01-08 23:36:43 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const NODE_ENV = process.env.NODE_ENV || "development";
+const webpack = require("webpack");
+
+module.exports = {
+	context: __dirname + "/src",
+	entry: {
+		selectedgoods: "./selectedgoods.jsx",
+	},
+	output: {
+		path: __dirname + "/scripts",
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [
+			{
+				loader: "babel",
+				test: /.jsx?$/,
+				include: [__dirname + "/src"],
+				query: {
+					presets: ["es2015", "react"]
+				}
+			}
+		]
+	},
+	watch: NODE_ENV == "development",
+/*
+	watchOptions: {
+		aggregateTimeout: 300
+	},
+*/
+	devtool: NODE_ENV == "development" ? "cheap-inline-source-map" : null,
+	plugins: NODE_ENV == "development" ? [
+	] : [
+		new webpack.optimize.UglifyJsPlugin(/*{
+			compress: {
+				keep_fnames: true,
+				unused: false,
+			}
+		}*/),
+	],
+}
